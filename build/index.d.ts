69c8e3b4b76a7b734051f70b213e8750b841f466
Peter Akaliro <pin4eva@hotmail.com>
2021-02-23 20:39:09 +0100

    init

---
+++ build/index.d.ts
@@ -0,0 +1,2 @@
+export * from "./types/Report.type";
+export * from "./types/User.type";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// import "./types/Report.type";
+// import "./types/User.type";
+
+export * from "./types/Report.type";
+export * from "./types/User.type";
